486e440daa1de6ffb297b95ce35816e9715ae6be
Michael Alpert-Appell <mikeappell@gmail.com>
2017-12-06 18:23:34 -0500

    Improve DataPoints API controller

--- app/controllers/api/v1/data_points_controller.rb
+++ app/controllers/api/v1/data_points_controller.rb
@@ -1,7 +1,12 @@
-class DataPointsController < Api::V1::ApiController
-  NUMBER_OF_DATAPOINTS = 20
+class Api::V1::DataPointsController < Api::V1::ApiController
+  NUMBER_OF_DATAPOINTS = 12
 
   def index
-    DataPoint.order(created_at: :desc).first(NUMBER_OF_DATAPOINTS)
+    data_points = DataPoint.order(dt: :asc)
+                           .limit(NUMBER_OF_DATAPOINTS)
+                           .select("distinct on (dt) *")
+                           .as_json(only: [:main_temp], methods: [:date])
+
+    render json: data_points
   end
 end
