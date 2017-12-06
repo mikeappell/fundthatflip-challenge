c4ecef9a0243f1464e30a3aa0ba1f386f0acc6ff
Michael Alpert-Appell <mikeappell@gmail.com>
2017-12-06 18:27:14 -0500

    Setup React-vis chart

--- app/javascript/components/ChartAndReport.jsx
+++ app/javascript/components/ChartAndReport.jsx
@@ -1,15 +1,52 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import jQuery from 'jquery';
+import { XYPlot, XAxis, YAxis, HorizontalGridLines, LabelSeries, LineSeries } from 'react-vis';
+
+window.jQuery = jQuery;
+window.$ = jQuery;
 
 export default class ChartAndReport extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { weatherData: [] }
+  }
+
   static propTypes = {
     dataPointsUrl: PropTypes.string.isRequired,
+  }
+
+  componentDidMount = () => {
+    this.getWeatherData();
+  }
+
+  formatTickLabel = (t, i) => {
+    return this.state.weatherData.filter((item) => { return item.x === i })[0].label;
+  }
+
+  getWeatherData = () => {
+    $.ajax({
+      type: 'GET',
+      url: this.props.dataPointsUrl,
+      success: (data) => {
+        const weatherData = data.map((datum, i) => { return { x: i, y: datum.main_temp, label: datum.date } });
+        this.setState({ weatherData });
+      }
+    })
   }
 
   render() {
     return (
       <div>
-        We React now, fam.
+        <XYPlot
+          width={1200}
+          height={300}>
+          <HorizontalGridLines />
+          <LineSeries
+            data={this.state.weatherData}/>
+          <XAxis tickFormat={this.formatTickLabel} tickTotal={this.state.weatherData.length}/>
+          <YAxis />
+        </XYPlot>
       </div>
     )
   }
