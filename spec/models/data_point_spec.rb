2db2428f0f958c6018fa822975a77bd0b61d125f
Michael Alpert-Appell <mikeappell@gmail.com>
2017-12-07 14:47:21 -0500

    Fix broken specs

--- spec/models/data_point_spec.rb
+++ spec/models/data_point_spec.rb
@@ -3,9 +3,9 @@ require 'rails_helper'
 RSpec.describe DataPoint, type: :model do
   let(:test_api_data) { HashWithIndifferentAccess.new(coord: { lon: -74.01, lat: 40.71 }, weather: [{ id: 300, main: 'Drizzle', description: 'light intensity drizzle', icon: '09d' }], base: 'stations', main: { temp: 55.56, pressure: 1022, humidity: 87, temp_min: 53.6, temp_max: 57.2 }, visibility: 16093, wind: { speed: 4.7, gust: 7.7, deg: 120 }, clouds: { all: 90 }, dt: 1512489360, sys: { type: 1, id: 2121, message: 0.0151, country: 'US', sunrise: 1512475531, sunset: 1512509312 }, id: 5128581, name: 'New York', cod: 200) }
 
-  describe '#initialize_from_api_data_point' do
+  describe '#find_or_initialize_from_api_data_point' do
     it 'initializes a new DataPoint from API-provided data' do
-      data_point = DataPoint.initialize_from_api_data_point(test_api_data)
+      data_point = DataPoint.find_or_initialize_from_api_data_point(test_api_data)
       expect(data_point.save!).to be_truthy
 
       DataPoint::ATTRIBUTE_API_MAP.each do |attr, api_attr|
@@ -14,13 +14,13 @@ RSpec.describe DataPoint, type: :model do
     end
   end
 
-  describe '#date' do
-    it "converts a DataPoint's 'dt' attribute to a DateTime" do
+  describe '#date_and_time' do
+    it "converts a DataPoint's 'dt' attribute to a date/time string" do
       test_epoch_date = 1512502562
-      expected_datetime = DateTime.parse('Tue, 05 Dec 2017 14:36:02 -0500')
+      expected_datetime = "12/05/2017 14:36"
 
       data_point = DataPoint.new(dt: test_epoch_date)
-      expect(data_point.date).to eq expected_datetime
+      expect(data_point.date_and_time).to eq expected_datetime
     end
   end
 end
