77309bdc0d8386c5f201ab92ad0f7208f9796daa
Michael Alpert-Appell <mikeappell@gmail.com>
2017-12-06 22:21:37 -0500

    Improve API controller

--- app/controllers/api/v1/data_points_controller.rb
+++ app/controllers/api/v1/data_points_controller.rb
@@ -1,16 +1,18 @@
 class Api::V1::DataPointsController < Api::V1::ApiController
-  NUMBER_OF_DATAPOINTS = 12
+  NUMBER_OF_DATAPOINTS = 20
 
   def index
-    data_points = DataPoint.order(dt: :asc)
-                           .limit(NUMBER_OF_DATAPOINTS)
-                           .select('distinct on (dt) *')
+    data_points = DataPoint.order(dt: :desc)
+                           .first(NUMBER_OF_DATAPOINTS)
+                           .reverse
                            .as_json(only: %i[main_temp
                                              main_humidity
                                              main_pressure
                                              visibility
                                              wind_speed],
-                                    methods: %i[date weather])
+                                    methods: %i[date_and_time
+                                                time
+                                                weather])
 
     render json: data_points
   end
